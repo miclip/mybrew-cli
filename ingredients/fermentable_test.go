ee89116632a12d695d7b7e7dc2cbbf9c5a96cc94
Michael Lipscombe <mlipscombe@outlook.com>
2017-08-04 21:32:44 -0600

    Added Fermentable local calculations spec

--- ingredients/fermentable_test.go
+++ ingredients/fermentable_test.go
@@ -8,15 +8,26 @@ import (
 )
 
 var _ = Describe("Fermentable", func() {
-	It("Prints the fermentable details", func() {
-		f := Fermentable{
-			Name:      "2 Row",
-			Amount:    23.35,
-			Potential: 1.036,
-			Yield:     77.9,
-			Lovibond:  2,
-		}
-		Ω(f).ShouldNot(BeNil())
-
+	Context("Fermentable Calculations", func() {
+		var f Fermentable
+		BeforeEach(func() {
+			f = Fermentable{
+				Name:      "2 Row",
+				Amount:    23.35,
+				Potential: 1.036,
+				Yield:     77.9,
+				Lovibond:  2,
+			}
+		})
+		It("Calculates the Points()", func() {
+			Ω(f.Points()).Should(Equal(35.834))
+		})
+		It("Calculates the PointsByAmount()", func() {
+			Ω(f.PointsByAmount()).Should(Equal(836.7239000000001))
+		})
+		It("Calculates the ColorMCU()", func() {
+			Ω(f.ColorMCU()).Should(Equal(46.7))
+		})
 	})
+
 })
